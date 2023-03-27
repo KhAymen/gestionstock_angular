9644d504fb5c99619ff7a784ce0e8ce8174c1176
Khekifi Aymen <aykhelifi@sqli.com>
2023-03-27 15:54:12 +0000

    Interface categories

---
+++ src/app/pages/categories/nouvelle-categorie/nouvelle-categorie.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-nouvelle-categorie',
+  templateUrl: './nouvelle-categorie.component.html',
+  styleUrls: ['./nouvelle-categorie.component.scss']
+})
+export class NouvelleCategorieComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  cancel():void {
+    this.router.navigate(['categories']);
+
+  }
+
+}
